7966ffecabaa2d0a74f0d9fdb1717c08b39bd2ba
unknown <mel.kj.chan@gmail.com>
2024-04-21 17:10:09 -0400

    Create collision

--- player.gd
+++ player.gd
@@ -37,3 +37,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y < 0
+
+
+func _on_body_entered(body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
